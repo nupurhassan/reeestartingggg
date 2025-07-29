01849c85df4c3ccef26f169dc6a42d15401802bc
nupurhassan <nupur.hassan3@gmail.com>
2025-07-28 19:25:31 -0700

    Save

--- twosum.py
+++ twosum.py
@@ -3,6 +3,7 @@ class Solution(object):
         numMap = {}
         for i, num in enumerate(nums):
             numMap[num] = i
+            #comment nupur v1 check
         for i, num in enumerate(nums):
             complement = target - num
             if complement in numMap and numMap[complement] != i:
