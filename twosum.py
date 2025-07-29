0f33b58662265c05121c459b0e2a960be1985dd6
nupurhassan <nupur.hassan3@gmail.com>
2025-07-28 19:14:20 -0700

    comment n1

---
+++ twosum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        numMap = {}
+        for i, num in enumerate(nums):
+            numMap[num] = i
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+        return []
+
+
+sol = Solution()
+nums = [2, 7, 11, 15]
+target = 9
+result = sol.twoSum(nums, target)
+print(result)
